367a9d98b9575f762f8a913acd27019621783276
Murph <murphdasmurf@users.noreply.github.com>
2018-03-09 17:55:48 -0800

    Second challenge
    https://cryptopals.com/sets/1/challenges/2

---
+++ 2.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+)
+
+func main() {
+	const s1 = "1c0111001f010100061a024b53535009181c"
+	const s2 = "686974207468652062756c6c277320657965"
+	byte1, err := hex.DecodeString(s1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	byte2, err := hex.DecodeString(s2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	xor_byte := xor(byte1, byte2)
+	xor_str := hex.EncodeToString(xor_byte)
+	fmt.Printf("%s\n", xor_str)
+}
+
+func xor(a []byte, b []byte) []byte {
+	c := make([]byte, len(a)+1)
+	for i := range b {
+		c[i] = b[i]
+	}
+	for i := range a {
+		c[i] ^= a[i]
+	}
+	return c
+}
