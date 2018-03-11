77f70a7514ae818598c93d4d5d049fb2d02895c2
Murph <murphdasmurf@users.noreply.github.com>
2018-03-10 21:42:25 -0800

    Arbitrary key length
    Rewrote XOR function to accept arbitrary length key without shitting the bed.

--- 3.go
+++ 3.go
@@ -16,43 +16,48 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Split up the letters to try with XOR.
+	// Split up the above ASCII characters to try with XOR.
 	ascii_array := strings.Split(Alphabet, "")
 	for _, letter := range ascii_array {
 		byte2 := []byte(letter)
 		xored_slice := xor(byte1, byte2)
-		fmt.Printf("%s\n", string(xored_slice[:]))
+		fmt.Println(string(xored_slice))
 	}
 }
 
 // Returns the integer length of the longer slice.
 func max_len(a []byte, b []byte) int {
 	if len(a) < len(b) {
-		fmt.Println(len(b))
 		return len(b)
 	}
 	return len(a)
 }
 
-// Determines the shorter slice (assumed 1 byte) and XORs the longer with it.
+// Determines the shorter slice and repeatedly XORs the longer with it.
 func xor(a []byte, b []byte) []byte {
-	// Make the slice to hold the XOR the length of the longer slice.
+	// Make a slice the length of the longer slice to hold the XORed value.
 	xored := make([]byte, max_len(a, b), max_len(a, b))
 	if len(a) < len(b) {
-		// Copy in the value of the longer slice XORed with the single byte other value.
 		for i := range b {
-        		xored[i] = b[i] ^ a[0]
+			// Operate in block the length of the shorter slice.
+			if i%len(a) != 0 {
+				continue
+			} else {
+				for n := 0; n < len(a); n++ {
+					xored[i+n] = b[i+n] ^ a[n]
+				}
+			}
 		}
-	}
-	// Do it all again in reverse!
-	if len(b) < len(a) {
+	} else { // Must be len(b) <= len(a), so do the above the other way around.
 		for i := range a {
-        		xored[i] = a[i] ^ b[0]
+			if i%len(b) != 0 {
+				continue
+			} else {
+				for n := 0; n < len(b); n++ {
+					xored[i+n] = a[i+n] ^ b[n]
+				}
+			}
 		}
-	}
-	// If equal, both must be one byte.
-	if len(a) == len(b) {
-		xored[0] = a[0] ^ b[0]
-	}
+	} 
 	return xored
 }
