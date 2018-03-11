2ac98a4482ca5b7dbeaedd8c5ef9c2475598ecfd
Murph <murphdasmurf@users.noreply.github.com>
2018-03-10 17:51:07 -0800

    Function to handle strings
    Hexor to handle strings, also updated comments to refer to the byte slices as slices and not arrays.

--- 2.go
+++ 2.go
@@ -9,6 +9,26 @@ import (
 func main() {
 	const s1 = "1c0111001f010100061a024b53535009181c"
 	const s2 = "686974207468652062756c6c277320657965"
+	fmt.Println(hexor(s1, s2))
+}
+
+// XORs 2 byte slices, assuming that both arguments are of equal length.
+func xor(a []byte, b []byte) []byte {
+	if len(a) != len(b) {
+		log.Fatal("Slices not equal length!")
+	}
+	c := make([]byte, len(a)+1)
+	for i := range b {
+		c[i] = b[i]
+	}
+	for i := range a {
+		c[i] ^= a[i]
+	}
+	return c
+}
+
+// XORs two hex strings using the function above.
+func hexor(s1, s2 string) string {
 	byte1, err := hex.DecodeString(s1)
 	if err != nil {
 		log.Fatal(err)
@@ -18,21 +38,6 @@ func main() {
 		log.Fatal(err)
 	}
 	xor_byte := xor(byte1, byte2)
-	xor_str := hex.EncodeToString(xor_byte)
-	fmt.Printf("%s\n", xor_str)
-}
-
-// XORs 2 byte arrays, assuming that both arguments are of equal length.
-// TODO: check this assumption and throw error if not true.
-// Consider accepting an optional third argument for position within the
-// longer array to XOR the shorter array against.
-func xor(a []byte, b []byte) []byte {
-	c := make([]byte, len(a)+1)
-	for i := range b {
-		c[i] = b[i]
-	}
-	for i := range a {
-		c[i] ^= a[i]
-	}
-	return c
+	xor_hex := hex.EncodeToString(xor_byte)
+	return xor_hex
 }
