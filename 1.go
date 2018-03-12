b5e6f42bd766586cc33dbffb0869bdbd9409cd4d
Murph <murphdasmurf@users.noreply.github.com>
2018-03-11 21:36:36 -0700

    Clean up
    Kill some whitespace, remove some redundant variables.

--- 1.go
+++ 1.go
@@ -8,12 +8,9 @@ import (
 )
 
 func main() {
-	const hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	raw, err := hex.DecodeString(hex_str)
+	raw, err := hex.DecodeString("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 	if err != nil {
 		log.Fatal(err)
 	}
-	b64_str := base64.StdEncoding.EncodeToString(raw)
-	fmt.Println(b64_str)
-
+	fmt.Println(base64.StdEncoding.EncodeToString(raw))
 }
