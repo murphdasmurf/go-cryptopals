6ec925d456a71bcc96bc9dce1c79409090a6d050
Murph <murphdasmurf@users.noreply.github.com>
2018-03-25 08:51:20 -0700

    More readable XOR
    Update to the more readable XOR, which uses min_len. Also technically was treating the strings wrong, it's just one with a newline - fixed that.

--- 5.go
+++ 5.go
@@ -6,14 +6,9 @@ import (
 )
 
 func main() {
-	const s1 = "Burning 'em, if you ain't quick and nimble"
-	const s2 = "I go crazy when I hear a cymbal"
+	const s1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 	const key = "ICE"
-	fmt.Println(hex.EncodeToString([]byte(key)))
-	fmt.Println(hex.EncodeToString([]byte(s1)))
 	fmt.Println(hex.EncodeToString(xor([]byte(s1), []byte(key))))
-	fmt.Println(hex.EncodeToString([]byte(s2)))
-	fmt.Println(hex.EncodeToString(xor([]byte(s2), []byte(key))))
 }
 
 // Returns the integer length of the longer slice.
@@ -24,33 +19,37 @@ func max_len(a []byte, b []byte) int {
 	return len(a)
 }
 
+// Returns the integer length of the shorter slice.
+func min_len(a []byte, b []byte) int {
+	if len(a) > len(b) {
+		return len(b)
+	}
+	return len(a)
+}
+
 // Determines the shorter slice and repeatedly XORs the longer with it.
 func xor(a []byte, b []byte) []byte {
 	// Make a slice the length of the longer slice to hold the XORed value.
-	xored := make([]byte, max_len(a, b), max_len(a, b))
+	result := make([]byte, max_len(a, b), max_len(a, b))
+	longer := make([]byte, max_len(a, b), max_len(a, b))
+	shorter := make([]byte, min_len(a, b), min_len(a, b))
 	if len(a) < len(b) {
-		for i := range b {
-			// Operate in block the length of the shorter slice.
-			if i%len(a) == 0 {
-				for n := 0; n < len(a); n++ {
-					if (i + n < len(b)) {
-						xored[i+n] = b[i+n] ^ a[n]
-					} else { continue }
-				}
-			} else { continue }
-		}
-	} else { // Must be len(b) <= len(a), so do the above the other way around.
-		for i := range a {
-			if i%len(b) == 0 {
-				for n := 0; n < len(b); n++ {
-					if (i + n < len(a)) {
-						xored[i+n] = a[i+n] ^ b[n]
-					} else {
-						continue
-					}
-				}
-			} else { continue }
-		}
-	} 
-	return xored
+	  copy(shorter, a)
+	  copy(longer, b)
+	} else {
+	  copy(shorter, b)
+	  copy(longer, a)
+	}
+	// Iterate along in blocks of length shorter.
+	for i := 0; i < len(longer); i += len(shorter) {
+		for n := 0; n < len(shorter); n++ {
+	   		if (i + n >= len(longer)) {
+			  	//fmt.Printf("XOR: I'm out! %d >= %d \n", i+n, len(longer))
+				  break
+			  } else {
+				  result[i+n] = longer[i+n] ^ shorter[n]
+			  }
+		  } // End iterating through characters in key.
+	} // End iterating through ciphertext.
+	return result
 }
